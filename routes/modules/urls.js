85d2d76deb4a8984c7af68fb31a041fd3f7fff8e
aziz0916 <aziz0916@yahoo.com.tw>
2022-01-21 10:33:05 +0800

    feat: modify post function

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -15,13 +15,9 @@ router.post('/', (req, res) => {
       for (let i = 0; i < items.length; i++) {
         shortUrlArray = shortUrlArray.concat(items[i].short_url)
       }
-    })
-    .catch(error => {
-      console.log(error)
-      res.render('errorPage', { status: 500, error: error.message })
-    })
 
-  return Url.findOne({ url })
+      return Url.findOne({ url })
+    })
     .then(item => {
       // 輸入新網址時，產生新的縮址
       if (!item) {
@@ -31,6 +27,7 @@ router.post('/', (req, res) => {
         while (shortUrlArray.includes(short_url)) {
           short_url = shortUrlGenerator()
         }
+
         return Url.create({ url, short_url })
           .then(() => {
             return Url.findOne({ url })
@@ -39,10 +36,6 @@ router.post('/', (req, res) => {
                 console.log(error)
                 res.render('errorPage', { status: 500, error: error.message })
               })
-          })
-          .catch(error => {
-            console.log(error)
-            res.render('errorPage', { status: 500, error: error.message })
           })
       }
       // 輸入相同網址時，從資料庫找出縮址，並導向detail頁面
@@ -57,7 +50,7 @@ router.post('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const id = req.params.id
 
-  return Url.findById(id)
+  Url.findById(id)
     .lean()
     .then(item => {
       const keyword = `http://localhost:3000/urlShortener.herokuapp.com/${item.short_url}`
